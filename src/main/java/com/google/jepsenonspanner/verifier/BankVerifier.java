a6217fc03811f81091316efe2ace7c4f50413756
tonyzhc <hanchiz@google.com>
2020-06-16 02:24:01 +0000

    Adding tests and comments for verifier

--- src/main/java/com/google/jepsenonspanner/verifier/BankVerifier.java
+++ src/main/java/com/google/jepsenonspanner/verifier/BankVerifier.java
@@ -1,5 +1,6 @@
 package com.google.jepsenonspanner.verifier;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.jepsenonspanner.loadgenerator.BankLoadGenerator;
 import us.bpsm.edn.Keyword;
 import us.bpsm.edn.parser.Parseable;
@@ -15,53 +16,123 @@ import java.util.List;
 import java.util.Map;
 
 public class BankVerifier implements Verifier {
+  // Workloads specific to the bank benchmark
   private static Keyword READ = Keyword.newKeyword(BankLoadGenerator.READ_LOAD_NAME);
   private static Keyword TRANSFER = Keyword.newKeyword(BankLoadGenerator.TRANSFER_LOAD_NAME);
 
   @Override
-  public void verify(String path, Map<String, Long> state) {
+  public boolean verify(String path, Map<String, Long> state) {
     try {
       FileReader fs = new FileReader(new File(path));
-      verify(fs, state);
+      return verify(fs, state);
     } catch (FileNotFoundException e) {
-      e.printStackTrace();
+      throw new RuntimeException("Invalid file");
     }
   }
 
-  void verify(Readable input, Map<String, Long> state) {
-    try {
-      Parseable pbr = Parsers.newParseable(input);
-      Parser parser = Parsers.newParser(Parsers.defaultConfiguration());
-      List<Map<Keyword, Object>> records = (List<Map<Keyword, Object>>) parser.nextValue(pbr);
+  @VisibleForTesting
+  boolean verify(Readable input, Map<String, Long> state) {
+    Parseable pbr = Parsers.newParseable(input);
+    Parser parser = Parsers.newParser(Parsers.defaultConfiguration());
 
+    // parses the edn file to Java readable data structure
+    List<Map<Keyword, Object>> records = (List<Map<Keyword, Object>>) parser.nextValue(pbr);
+
+    try {
       for (Map<Keyword, Object> record : records) {
         if (record.get(TYPE) == OK) {
-          Keyword opName = (Keyword) record.get(OP_NAME);
-          List<String> value = (List<String>) record.get(VALUE);
-          if (opName.equals(READ)) {
-            Map<String, Long> currentState = new HashMap<>();
-            for (String representation : value) {
-              String[] keyValues = representation.split(" ");
-              currentState.put(keyValues[0], Long.parseLong(keyValues[1]));
-            }
-            if (!currentState.equals(state)) {
-              System.out.printf("Invalid history found at operation: %s %s", opName.getName(),
-                      value);
-            }
-          } else if (opName.equals(TRANSFER)) {
-            String[] transferParams = value.get(0).split(" ");
-            String fromAcct = transferParams[0];
-            String toAcct = transferParams[1];
-            long amount = Long.parseLong(transferParams[2]);
-            state.put(fromAcct, state.get(fromAcct) - amount);
-            state.put(toAcct, state.get(toAcct) + amount);
-          }
+          checkOk(record, state);
+        } else if (record.get(TYPE) == FAIL) {
+          checkFail(record, state);
         }
       }
+    } catch (VerifierException e) {
+      System.out.println("Invalid operation found at " + e.getMessage());
+      return false;
+    }
 
-      System.out.println("Valid!");
-    } catch (ClassCastException e) {
-      e.printStackTrace();
+    System.out.println("Valid!");
+    return true;
+  }
+
+  /**
+   * Given an "ok" record and the current state of the database according to previous records,
+   * determine if this record is valid. Throws a VerifierException if it is invalid.
+   */
+  private void checkOk(Map<Keyword, Object> record, Map<String, Long> state)
+          throws VerifierException {
+    Keyword opName = (Keyword) record.get(OP_NAME);
+    List<String> value = (List<String>) record.get(VALUE);
+    if (opName.equals(READ)) {
+      checkOkRead(value, state);
+    } else if (opName.equals(TRANSFER)) {
+      checkOkTransfer(value, state);
+    }
+  }
+
+  /**
+   * Checks balances read from each bank account and verifies against the current state of the
+   * database. Throws a VerifierException if it is invalid.
+   * @param value a list of values read in the format of [["0" 20] ["1" 15]] i.e. read balance of
+   *             account "0" = 20, account "1" = 15
+   * @param state a map of current state given all previous records
+   */
+  private void checkOkRead(List<String> value, Map<String, Long> state) throws VerifierException {
+    Map<String, Long> currentState = new HashMap<>();
+    for (String representation : value) {
+      String[] keyValues = representation.split(" ");
+      currentState.put(keyValues[0], Long.parseLong(keyValues[1]));
+    }
+    if (!currentState.equals(state)) {
+      throw new VerifierException(READ.getName(), value);
+    }
+  }
+
+  /**
+   * Updates the state of the database given a successful transfer between 2 accounts.
+   * @param value a list of values read in the format of [["0" "1" 15]] i.e. transfer 15 from
+   *              account "0" to account "1"
+   * @param state a map of current state given all previous records
+   */
+  private void checkOkTransfer(List<String> value, Map<String, Long> state) {
+    String[] transferParams = value.get(0).split(" ");
+    String fromAcct = transferParams[0];
+    String toAcct = transferParams[1];
+    long amount = Long.parseLong(transferParams[2]);
+    state.put(fromAcct, state.get(fromAcct) - amount);
+    state.put(toAcct, state.get(toAcct) + amount);
+  }
+
+  /**
+   * Given a "fail" record and the current state of the database according to previous records,
+   * determine if this record is valid. Throws a VerifierException if it is invalid.
+   */
+  private void checkFail(Map<Keyword, Object> record, Map<String, Long> state) throws VerifierException {
+    Keyword opName = (Keyword) record.get(OP_NAME);
+    List<String> value = (List<String>) record.get(VALUE);
+    if (opName.equals(TRANSFER)) {
+      checkFailTransfer(value, state);
+    } else {
+      // a read operation should not produce a fail record
+      throw new VerifierException(opName.getName(), value);
+    }
+  }
+
+  /**
+   * Checks if a failed transfer between 2 accounts should actually fail according to current
+   * state of the database.
+   * @param value a list of values read in the format of [["0" "1" 15]] i.e. transfer 15 from
+   *              account "0" to account "1"
+   * @param state a map of current state given all previous records
+   */
+  private void checkFailTransfer(List<String> value, Map<String, Long> state)
+          throws VerifierException {
+    String[] transferParams = value.get(0).split(" ");
+    String fromAcct = transferParams[0];
+    long amount = Long.parseLong(transferParams[2]);
+    if (state.get(fromAcct) >= amount) {
+      // amount should be transferable
+      throw new VerifierException(TRANSFER.getName(), value);
     }
   }
 }
--- src/main/java/com/google/jepsenonspanner/verifier/Verifier.java
+++ src/main/java/com/google/jepsenonspanner/verifier/Verifier.java
@@ -5,7 +5,11 @@ import us.bpsm.edn.Keyword;
 
 import java.util.Map;
 
+/**
+ * Each type of workload must implement this interface to check against a history file
+ */
 public interface Verifier {
+  // Pre-defined field names for the history file
   static final Keyword PROCESS = Keyword.newKeyword("process");
   static final Keyword VALUE = Keyword.newKeyword("value");
   static final Keyword TYPE = Keyword.newKeyword("type");
@@ -15,5 +19,11 @@ public interface Verifier {
   static final Keyword FAIL = Keyword.newKeyword(Executor.FAIL_STR);
   static final Keyword INFO = Keyword.newKeyword(Executor.INFO_STR);
 
-  public void verify(String path, Map<String, Long> initialState);
+
+  /**
+   * Given the path to a history file and a map of initial state of the database, check if the
+   * history file reflects a consistency error the benchmark is looking for. Returns false if
+   * there is an error, and prints the location of the error to System.out.
+   */
+  public boolean verify(String path, Map<String, Long> initialState);
 }
