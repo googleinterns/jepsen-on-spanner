197715efe3a444e0f5a660b90ed141bb14fd3f54
tonyzhc <hanchiz@google.com>
2020-06-16 02:37:21 +0000

    Revert "Adding tests and comments for verifier"
    This reverts commit a6217fc03811f81091316efe2ace7c4f50413756.

--- src/main/java/com/google/jepsenonspanner/verifier/Verifier.java
+++ src/main/java/com/google/jepsenonspanner/verifier/Verifier.java
@@ -5,11 +5,7 @@ import us.bpsm.edn.Keyword;
 
 import java.util.Map;
 
-/**
- * Each type of workload must implement this interface to check against a history file
- */
 public interface Verifier {
-  // Pre-defined field names for the history file
   static final Keyword PROCESS = Keyword.newKeyword("process");
   static final Keyword VALUE = Keyword.newKeyword("value");
   static final Keyword TYPE = Keyword.newKeyword("type");
@@ -19,11 +15,5 @@ public interface Verifier {
   static final Keyword FAIL = Keyword.newKeyword(Executor.FAIL_STR);
   static final Keyword INFO = Keyword.newKeyword(Executor.INFO_STR);
 
-
-  /**
-   * Given the path to a history file and a map of initial state of the database, check if the
-   * history file reflects a consistency error the benchmark is looking for. Returns false if
-   * there is an error, and prints the location of the error to System.out.
-   */
-  public boolean verify(String path, Map<String, Long> initialState);
+  public void verify(String path, Map<String, Long> initialState);
 }
--- src/test/java/com/google/jepsenonspanner/verifier/BankVerifierTest.java
+++ src/test/java/com/google/jepsenonspanner/verifier/BankVerifierTest.java
@@ -1,33 +1,25 @@
 package com.google.jepsenonspanner.verifier;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import us.bpsm.edn.parser.Parseable;
 import us.bpsm.edn.parser.Parsers;
 
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankVerifierTest {
-  private Map<String, Long> state;
 
-  @BeforeEach
-  void setUp() {
-    state = Map.of(
+  @Test
+  void verify() {
+    BankVerifier verifier = new BankVerifier();
+    Map<String, Long> state = Map.of(
             "0", 20L,
             "1", 20L,
             "2", 20L,
             "3", 20L,
             "4", 20L);
-  }
-
-  @Test
-  void verify() {
-    BankVerifier verifier = new BankVerifier();
-    String input = "[{:type :invoke, :f :read, :value []}]";
-    verifier.verify(new StringReader(input), state);
+    verifier.verify("history.edn", new HashMap<>(state));
   }
 }
