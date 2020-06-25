c4825b1459b005d50d51bc5ef568e40fc3ab2e9f
tonyzhc <hanchiz@google.com>
2020-06-25 01:53:16 +0000

    Building linearizability generator...

--- src/main/java/com/google/jepsenonspanner/loadgenerator/LinearizabilityLoadGenerator.java
+++ src/main/java/com/google/jepsenonspanner/loadgenerator/LinearizabilityLoadGenerator.java
@@ -1,14 +1,19 @@
 package com.google.jepsenonspanner.loadgenerator;
 
 import com.google.jepsenonspanner.operation.Operation;
+import com.google.jepsenonspanner.operation.ReadTransaction;
+import com.google.jepsenonspanner.operation.ReadWriteTransaction;
 
 import java.util.List;
+import java.util.Random;
 
 public class LinearizabilityLoadGenerator extends LoadGenerator {
   private List<String> keys;
   private boolean allowMultiKeys;
   private boolean allowMixedReadsWrites;
   private boolean allowCAS;
+  private Random rand;
+  private Config config;
 
   public static class Config extends LoadRatioConfig {
     private int read;
@@ -42,16 +47,45 @@ public class LinearizabilityLoadGenerator extends LoadGenerator {
   }
 
   public LinearizabilityLoadGenerator(int opLimit, List<String> keys, boolean allowMultiKeys,
-                                      boolean allowMixedReadsWrites, boolean allowCAS) {
+                                      boolean allowMixedReadsWrites, boolean allowCAS,
+                                      int ... loadRatios) {
     super(opLimit);
     this.keys = keys;
     this.allowMultiKeys = allowMultiKeys;
     this.allowMixedReadsWrites = allowMixedReadsWrites;
     this.allowCAS = allowCAS;
+    this.config = new Config(loadRatios);
   }
 
   @Override
   public Operation nextOperation() {
+
+    // check if reached limit
+    if (opLimit <= 0) {
+      throw new RuntimeException("Bank generator has reached limit");
+    }
+
+    opLimit--;
+    int nextOp = rand.nextInt();
+    switch (config.categorizeLinearizabilityLoad(nextOp)) {
+      case READ:
+        return read();
+      case WRITE:
+        return write();
+      default:
+        return cas();
+    }
+  }
+
+  public ReadTransaction read() {
+    return null;
+  }
+
+  public ReadWriteTransaction write() {
+    return null;
+  }
+
+  public ReadWriteTransaction cas() {
     return null;
   }
 }
